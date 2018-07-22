cbd2c5a69f750d6b9785ecac0df68dbfd60cfa41
pudong <ccommons@163.com>
2018-07-22 19:56:36 +0800

    -madd clude url

--- resume/caseView.js
+++ resume/caseView.js
@@ -1,6 +1,6 @@
 var caseViwe = {
     init: function () {
-        caseViwe.industryClass();//--一级行业数据
+       // caseViwe.industryClass();//--一级行业数据
         caseViwe.event_(); //--全局事件
     },
     event_:function () {
@@ -27,38 +27,6 @@ var caseViwe = {
             }else{
                 e.cancelBubble=true;
             };
-        });
-    },
-    industryClass: function () {  //--一级行业数据
-        $.ajax({
-            type: "get",
-            url: "http://www.capabcv.com/resumev2/resumev2.ashx?resumeaction=classificationloadv2",
-            dataType: "jsonp",
-            jsonp: "callback",
-            success: function(data) {
-                var str = "";
-                var ele = "";
-                var ele2 = "";
-                var list1 = [];
-                for(var i = 0;i < data.classification2.length;i++){
-                    if(str != data.classification2[i].sClassification1){
-                        list1.push(data.classification2[i].sClassification1);
-                        ele += "<li class='list2'>"+"<span>"+data.classification2[i].sClassification1+"</span>"+"<i></i>"+"</li>";
-                    };
-                    str = data.classification2[i].sClassification1;
-                };
-                $("#industry").html(ele);
-                for(var j = 0;j<list1.length;j++){
-                    ele2 = "<ul>";
-                    for(var i = 0;i < data.classification2.length;i++){
-                        if(list1[j] == data.classification2[i].sClassification1){
-                            ele2 += "<li>"+data.classification2[i].sClassification2+"</li>";
-                        };
-                    };
-                    ele += "</ul>"
-                    $(".list2").eq(j).html($(".list2").eq(j).html()+ele2);
-                };
-            },
         });
     },
 };
